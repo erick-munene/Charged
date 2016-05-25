a3ec8abf91f816a85626a76f7e6adb350c8560f8
Erick-Munene <munenewahome@gmail.com>
2016-05-25 15:59:53 +0300

    added a splash screen

--- app/src/main/java/com/example/erick_king/charged/know.java
+++ app/src/main/java/com/example/erick_king/charged/know.java
@@ -1,7 +1,17 @@
 package com.example.erick_king.charged;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by erick-king on 25/05/16.
  */
-public class know {
+public class know extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
+
